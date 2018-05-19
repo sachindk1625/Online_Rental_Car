30860202a97cd2a80bf7f58f25343c595e636dd9
sachindk1625 <sachindk1625@gmail.com>
2018-05-18 21:37:12 -0500

    adding bookedCar

---
+++ Snappy1/Models/BookedCars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snappy1.Models
+{
+    public class BookedCars
+    {
+        public int BookingId { get; set; }
+
+        public DateTime BookingStartDate { get; set; }
+
+        public DateTime BookingEndtDate { get; set; }
+
+        public string CarModel { get; set; }
+
+        public string CarBrand { get; set; }
+
+        public int ProductionYear { get; set; }
+
+        public string CarColor { get; set; }
+
+        public string CategoryType { get; set; }
+
+    }
+}
